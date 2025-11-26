ad48d5a51f19fdc1dd81175afe4a41bdcdb3fc53
Akshat tiwari <a49011094@gmail.com>
2025-11-26 13:38:46 +0530

    Create que60.c

---
+++ que60.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Total even numbers = %d\n", even);
+    printf("Total odd numbers = %d\n", odd);
+
+    return 0;
+}
