4ab9c777fb276f2dfe698447f71890211053dc34
Akshat tiwari <a49011094@gmail.com>
2025-11-26 13:25:46 +0530

    Create que52.c

---
+++ que52.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("*\n");        // 1 star
+
+    printf("\n");         // blank line
+
+    printf("*\n*\n*\n");  // 3 stars
+
+    printf("\n");         // blank line
+
+    printf("*\n*\n*\n*\n*\n"); // 5 stars
+
+    printf("\n");         // blank line
+
+    printf("*\n*\n*\n");  // 3 stars
+
+    printf("\n");         // blank line
+
+    printf("*\n");        // 1 star
+
+    return 0;
+}
